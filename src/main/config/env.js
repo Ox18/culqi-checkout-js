6b6ef3ec9e42cb58d5bfad3ac120e8428a7ec725
Wilmer Delgado <wilmer.delgado@culqi.com>
2024-07-29 14:18:58 -0500

    fix: to re-deploy

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 const env = {
-  port: process.env.PORT || 3005,
+  port: process.env.PORT || 3000,
   private_key: process.env.PRIVATE_KEY || 'sk_xxxxxxxxxxxxxxx',
   public_key: process.env.PUBLIC_KEY || 'pk_xxxxxxxxxxxxxxx',
 };
