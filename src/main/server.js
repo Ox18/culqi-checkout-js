ec63fc5d37cdefc09d10cbad0b61c1717bb2cd8b
Wilmer Delgado <wilmer.delgado@culqi.com>
2024-07-29 14:22:20 -0500

    fix: to re-deploy

--- src/main/server.js
+++ src/main/server.js
@@ -1,14 +1,18 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const env = require("./config/env");
-const logger = require("../shared/libs/logger");
-const setupApp = require("./config/app");
+const env = require("../config/env");
+const logger = require("../../shared/libs/logger");
+const setupApp = require("../config/app");
 
 (async () => {
-  const app = await setupApp();
-  app.listen(env.port, () =>
-    logger.info(`Server running on http://localhost:${env.port}`)
-  );
+  try {
+    const app = await setupApp();
+    app.listen(env.port, () =>
+      logger.info(`Server running on http://localhost:${env.port}`)
+    );
+  } catch (error) {
+    logger.error("Failed to start the server", error);
+  }
 })();
 
 module.exports = app;
